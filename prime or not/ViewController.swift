bea90b28df92a201aa17ab2b352cf2dc379e95fb
admin <admin@VIT-CC03-iMac04.local>
2017-06-10 15:35:35 +0530

    prime or not

--- prime or not/ViewController.swift
+++ prime or not/ViewController.swift
@@ -10,16 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+    
+    
+    @IBOutlet weak var output: UILabel!
+
+    
+    @IBOutlet weak var input: UITextField!
 
     override func didReceiveMemoryWarning() {
+       
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+       
     }
 
+    @IBAction func check(_ sender: Any)
+    {
+    
+        var number = Int(input.text!)!
+        var isprime = true
+        if number != 1
+        {
+            for i in 2..<number
+            {
+                if number % i == 0
+                {
+                    isprime=false
+                }
+            }
+        }
+        print(isprime)
+        
+        if isprime == true
+        {
+            output.text = "Prime"
+             }
+        else {
+            output.text = "Not Prime"
+        }
+    }
 
 }
 
